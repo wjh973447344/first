80b19e3ec1898f2e215f93cf3e8cd0084b859ca4
wangjiahui <Wangjiahui5510>
2022-07-19 22:51:19 +0800

    Changes

--- src/main/java/com/wjh/first/FirstApplication.java
+++ src/main/java/com/wjh/first/FirstApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FirstApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/wjh/first/test.java
@@ -0,0 +1,11 @@
+package com.wjh.first;
+
+
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("111");
+
+    }
+}
